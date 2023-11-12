d57812f2bdf8709a56facc5ef58713c6da96f295
adamrichardson14 <adamrichardson14@icloud.com>
2023-11-12 12:23:56 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,172 @@
-import Image from 'next/image'
+"use client";
 
+import { useEffect, useRef, useState } from "react";
+
+const WIDTH = 800;
+const HEIGHT = 800;
+const CELL_SIZE = 10;
+const NUM_ROWS = Math.floor(WIDTH / CELL_SIZE);
+const NUM_COLS = Math.floor(HEIGHT / CELL_SIZE);
+
+const colors = ["black", "white"];
+
+type Board = number[][];
+function createBoard(): Board {
+  return Array.from({ length: NUM_ROWS }, () => new Array(NUM_ROWS).fill(0));
+}
 export default function Home() {
+  const initialBoard = createBoard();
+  const [boardState, setBoardState] = useState<Board>(initialBoard);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const canvasRef = useRef<null | HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!isPlaying) {
+      return;
+    }
+    const interval = setInterval(computeNextBoardSeeds, 100);
+    return () => clearInterval(interval);
+  }, [isPlaying, computeNextBoardSeeds]);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d");
+      if (!ctx) return;
+
+      ctx.clearRect(0, 0, WIDTH, HEIGHT);
+      ctx.strokeStyle = "gray";
+      ctx.lineWidth = 0.1;
+
+      for (let r = 0; r < NUM_ROWS; ++r) {
+        for (let c = 0; c < NUM_COLS; ++c) {
+          ctx.fillStyle = colors[boardState[r][c]];
+
+          ctx.fillRect(
+            Math.floor((WIDTH / NUM_ROWS) * r),
+            Math.floor((HEIGHT / NUM_COLS) * c),
+            CELL_SIZE,
+            CELL_SIZE
+          );
+
+          ctx.strokeRect(
+            Math.floor((WIDTH / NUM_ROWS) * r),
+            Math.floor((HEIGHT / NUM_COLS) * c),
+            CELL_SIZE,
+            CELL_SIZE
+          );
+        }
+      }
+    }
+  }, [boardState, canvasRef]);
+
+  function countNbors(r0: number, c0: number) {
+    let count = 0;
+    for (let dr = -1; dr <= 1; ++dr) {
+      for (let dc = -1; dc <= 1; ++dc) {
+        if (dr != 0 || dc != 0) {
+          const r = (r0 + dr + NUM_ROWS) % NUM_ROWS;
+          const c = (c0 + dc + NUM_COLS) % NUM_COLS;
+
+          if (boardState[r][c] === 1) {
+            ++count;
+          }
+        }
+      }
+    }
+    return count;
+  }
+
+  function computeNextBoardGol() {
+    setBoardState((prevBoardState) => {
+      let newBoard = prevBoardState.map((row) => [...row]);
+
+      for (let r = 0; r < NUM_ROWS; ++r) {
+        for (let c = 0; c < NUM_COLS; ++c) {
+          const aliveCount = countNbors(r, c);
+          if (prevBoardState[r][c] === 0) {
+            if (aliveCount === 3) {
+              newBoard[r][c] = 1;
+            }
+          } else {
+            if (aliveCount !== 2 && aliveCount !== 3) {
+              newBoard[r][c] = 0;
+            }
+          }
+        }
+      }
+      return newBoard;
+    });
+  }
+
+  function computeNextBoardSeeds() {
+    setBoardState((prevBoardState) => {
+      let newBoard = prevBoardState.map((row) => [...row]);
+
+      for (let r = 0; r < NUM_ROWS; ++r) {
+        for (let c = 0; c < NUM_COLS; ++c) {
+          const aliveCount = countNbors(r, c);
+          if (prevBoardState[r][c] === 0) {
+            if (aliveCount === 2) {
+              newBoard[r][c] = 1;
+            }
+          } else {
+            newBoard[r][c] = 0;
+          }
+        }
+      }
+      return newBoard;
+    });
+  }
+
+  function resetBoard() {
+    const board = createBoard();
+    setBoardState(board);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <h1 className="text-3xl text-center font-mono mt-4">Game of Life</h1>
+
+      <div className="flex justify-center flex-col items-center">
+        <div className="flex space-x-3">
+          <button
+            className="py-4"
+            onClick={computeNextBoardSeeds}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Next
+          </button>
+          <button
+            className="py-4"
+            onClick={resetBoard}
+          >
+            Reset
+          </button>
+          <button
+            className="py-4"
+            onClick={() => setIsPlaying(!isPlaying)}
+          >
+            {isPlaying ? "Stop" : "Play"}
+          </button>
         </div>
+        <canvas
+          onClick={(e) => {
+            const x = Math.floor(e.nativeEvent.offsetX / CELL_SIZE);
+            const y = Math.floor(e.nativeEvent.offsetY / CELL_SIZE);
+
+            let updatedBoardState = [...boardState];
+            if (updatedBoardState[x][y] === 0) {
+              updatedBoardState[x][y] = 1;
+            } else {
+              updatedBoardState[x][y] = 0;
+            }
+            setBoardState(updatedBoardState);
+          }}
+          ref={canvasRef}
+          width={WIDTH}
+          height={HEIGHT}
+          className="bg-gray-900"
+        ></canvas>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
